92de4ccbae115e9604b990eba83642e8f2fca4a8
pvn-leo <pavan.leo36dna@gmail.com>
2020-09-10 23:43:33 +0530

    major debug changes

--- Assignment_2/check.py
+++ Assignment_2/check.py
@@ -1,4 +1,6 @@
+import sys
 
+debug = int(sys.argv[1])
 def get_neighbours(cost, v, n):
 	neigh = []
 	for i in range(1, n+1):
@@ -18,7 +20,7 @@ def astar(cost, heuristic, start_point, goals):
 	
 	g = {} # g(n) -> current distance from start_node to n
 	g[start_point] = 0
-	debug = 0
+	#debug = 1
 	parents = {}
 	parents[start_point] = start_point
 
@@ -48,12 +50,12 @@ def astar(cost, heuristic, start_point, goals):
 		if(n in goals):
 			orig = n
 			if(debug):
-				print("mincost: {0}, g[n]: {1}".format(mincost,g[n]))
-			if(mincost==-1 or mincost > g[n]):
+				print("mincost: {0}, g[n]+h[n]: {1}".format(mincost,g[n]+heuristic[n]))
+			if(mincost==-1 or mincost > g[n]+heuristic[n]):
 				if(debug):
 					print("in")
 				#print(g[n])
-				mincost = g[n]
+				mincost = g[n] + heuristic[n]
 				temp_soln = []
 				while parents[n] != n:
 					temp_soln.append(n)
@@ -66,6 +68,7 @@ def astar(cost, heuristic, start_point, goals):
 
 		# n->parent m->child
 		for (m, weight) in get_neighbours(cost, n, num ):
+			#print(m,weight)
 			if ( (m not in frontier) and (m not in explored)):
 				frontier.add(m)
 				parents[m] = n
@@ -86,7 +89,6 @@ def astar(cost, heuristic, start_point, goals):
 		if(debug):
 			print("after: FRONTIER -- ", frontier)
 			print("after: EXPLORED -- ", explored); print()
-
 	return temp_soln
 	return None
 
@@ -101,12 +103,22 @@ cost = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, -1, -1, -1, -1, 2, -1, -1, 0, -1, 8],
             [0, -1, -1, -1, -1, -1, -1, -1, -1, 0, 7],
             [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0]]
-heuristic = [0, 5, 7, 3, 4, 6, 0, 0, 6, 5, 0]
+cost1 = [[0,0,0,0,0,0,0,0],
+	[0,0,3,-1,-1,-1,-1,2],
+	[0,-1,0,5,10,-1,-1,-1],
+	[0,-1,-1,0,2,-1,1,-1],
+	[0,-1,-1,-1,0,4,-1,-1],
+	[0,-1,-1,-1,-1,0,-1,-1],
+	[0,-1,-1,-1,-1,3,0,-1],
+	[0,-1,-1,1,-1,-1,4,0]]
+heuristic1 = [0, 5, 7, 3, 4, 6, 0, 0, 6, 5, 0]
+heuristic2 = [0,7,9,4,2,0,3,5]
 #check = get_neighbours(cost, 1, 10)
 #for (m,weight)  in check:
 #	print(m,weight)
 
-print(astar(cost, heuristic, 1, [6,7,10]))
+print(astar(cost, heuristic1, 1, [8]))
+print(astar(cost1, heuristic2, 1, [2]))
 '''
 verifies with unit 1 (video on PESU) : Problem solving by Searching- Informed Search , time - 19:37
 for goal state 	6 - [1,2,6] g(n) = 14
